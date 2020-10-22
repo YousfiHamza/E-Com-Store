eb909dc45e4ae9f0620d9d83d66f8057169778a6
Hamza <hamzayousfi2@gmail.com>
2020-10-23 00:36:05 +0100

    Updates

---
+++ precache-manifest.eb06507fc3c73ede2cf3b72856836f82.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db1ab7a80db7b6be0a9829a5fd9133dc",
+    "url": "/E-Com-Store/index.html"
+  },
+  {
+    "revision": "f8cf6f0602788b34302f",
+    "url": "/E-Com-Store/static/css/main.4c02ad59.chunk.css"
+  },
+  {
+    "revision": "308ee927a0afdbd6eab7",
+    "url": "/E-Com-Store/static/js/2.5a4c42e5.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/E-Com-Store/static/js/2.5a4c42e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8cf6f0602788b34302f",
+    "url": "/E-Com-Store/static/js/main.a2ca4997.chunk.js"
+  },
+  {
+    "revision": "fad848de40219c7b1dfb",
+    "url": "/E-Com-Store/static/js/runtime-main.c664c56c.js"
+  },
+  {
+    "revision": "117749b888984f08fc7ce52cb56eb0e8",
+    "url": "/E-Com-Store/static/media/if.117749b8.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/E-Com-Store/static/media/shopping-bag.30f07071.svg"
+  }
+]);
