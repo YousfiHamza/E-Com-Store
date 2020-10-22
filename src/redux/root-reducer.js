0f1b4d8d1715e74910951b5b6825bff144e774ab
Hamza <hamzayousfi2@gmail.com>
2020-10-22 14:24:02 +0100

    persisting the data in our app using redux-persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage/session";
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whiteList: ["cart"], // no need to persist user because its handeled by Firebase
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
