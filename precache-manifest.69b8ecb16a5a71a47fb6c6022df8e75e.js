285f573c6bc879fef4a4a088bb188465f6465d78
Hamza <hamzayousfi2@gmail.com>
2020-11-06 00:34:12 +0100

    Updates

---
+++ precache-manifest.69b8ecb16a5a71a47fb6c6022df8e75e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3617b4616911f3c3df6406295dd7e8c5",
+    "url": "/E-Com-Store/index.html"
+  },
+  {
+    "revision": "27cbf5abe024368373e6",
+    "url": "/E-Com-Store/static/css/main.8ff59cd9.chunk.css"
+  },
+  {
+    "revision": "6e86cd791bcfb5a2c9c4",
+    "url": "/E-Com-Store/static/js/2.32b35811.chunk.js"
+  },
+  {
+    "revision": "c608fb72bff51f1d457ebafb893fb94c",
+    "url": "/E-Com-Store/static/js/2.32b35811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27cbf5abe024368373e6",
+    "url": "/E-Com-Store/static/js/main.78b2b1ad.chunk.js"
+  },
+  {
+    "revision": "fad848de40219c7b1dfb",
+    "url": "/E-Com-Store/static/js/runtime-main.c664c56c.js"
+  },
+  {
+    "revision": "a5a86f179f4af64b0b25366f320034db",
+    "url": "/E-Com-Store/static/media/YousfitnessLogo.a5a86f17.png"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/E-Com-Store/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "2c0663c1bd15ae55381e03234c909605",
+    "url": "/E-Com-Store/static/media/team1.2c0663c1.jpg"
+  },
+  {
+    "revision": "1e6799328bd18112242ccb5e05c77b27",
+    "url": "/E-Com-Store/static/media/team2.1e679932.jpg"
+  },
+  {
+    "revision": "c50c77465ea4c221cd3164fc11533c7a",
+    "url": "/E-Com-Store/static/media/team3.c50c7746.jpg"
+  },
+  {
+    "revision": "8b134bd92740a17d8ba8baaad87f067e",
+    "url": "/E-Com-Store/static/media/wave_1.8b134bd9.gif"
+  },
+  {
+    "revision": "bf19e90f6be153a541aea7b0d79216a9",
+    "url": "/E-Com-Store/static/media/wave_2.bf19e90f.png"
+  },
+  {
+    "revision": "26648e95ce1bd90d198e4b46d091f595",
+    "url": "/E-Com-Store/static/media/wave_3.26648e95.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/E-Com-Store/precache-manifest.a89e8c86bfd40ba3820353c0c37856e0.js"
+  "/E-Com-Store/precache-manifest.69b8ecb16a5a71a47fb6c6022df8e75e.js"
 );
 
 self.addEventListener('message', (event) => {
