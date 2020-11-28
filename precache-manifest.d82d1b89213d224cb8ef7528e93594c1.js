278227e2b28f23f7a789e92f9df82bd4f1315640
Hamza <hamzayousfi2@gmail.com>
2020-11-28 19:56:19 +0100

    Updates

---
+++ precache-manifest.d82d1b89213d224cb8ef7528e93594c1.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35060dd795b948e58ee57400925d24a7",
+    "url": "/YH-Clothing/index.html"
+  },
+  {
+    "revision": "ac008867f2d3a73bdcfa",
+    "url": "/YH-Clothing/static/css/4.9905d55a.chunk.css"
+  },
+  {
+    "revision": "028558325ccdd81ee134",
+    "url": "/YH-Clothing/static/css/main.8ff59cd9.chunk.css"
+  },
+  {
+    "revision": "46eb3b7b57d6f2643632",
+    "url": "/YH-Clothing/static/js/10.89105897.chunk.js"
+  },
+  {
+    "revision": "43b7785aae300f2ad0cb",
+    "url": "/YH-Clothing/static/js/2.78e50e16.chunk.js"
+  },
+  {
+    "revision": "c608fb72bff51f1d457ebafb893fb94c",
+    "url": "/YH-Clothing/static/js/2.78e50e16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f3362fbb4affaebb6c6",
+    "url": "/YH-Clothing/static/js/3.e38c63cb.chunk.js"
+  },
+  {
+    "revision": "ac008867f2d3a73bdcfa",
+    "url": "/YH-Clothing/static/js/4.27398afd.chunk.js"
+  },
+  {
+    "revision": "12e09b348da87042f9e9",
+    "url": "/YH-Clothing/static/js/5.2c44ab9e.chunk.js"
+  },
+  {
+    "revision": "0256c54e4e3d884123e4",
+    "url": "/YH-Clothing/static/js/6.b05984e5.chunk.js"
+  },
+  {
+    "revision": "d80a967d65562e8ac46b11937d3ba69e",
+    "url": "/YH-Clothing/static/js/6.b05984e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87c8fe9f3ed181339465",
+    "url": "/YH-Clothing/static/js/7.e17df77d.chunk.js"
+  },
+  {
+    "revision": "20c4661902676a7b0f77",
+    "url": "/YH-Clothing/static/js/8.e83a5d46.chunk.js"
+  },
+  {
+    "revision": "b8ef56d5f048d1a12e9e",
+    "url": "/YH-Clothing/static/js/9.0640fb2d.chunk.js"
+  },
+  {
+    "revision": "028558325ccdd81ee134",
+    "url": "/YH-Clothing/static/js/main.77a06edb.chunk.js"
+  },
+  {
+    "revision": "32f087dba79bf611660069a68c00c0b9",
+    "url": "/YH-Clothing/static/js/main.77a06edb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d15fec90a3cfc63b3723",
+    "url": "/YH-Clothing/static/js/runtime-main.b1a5052c.js"
+  },
+  {
+    "revision": "a5a86f179f4af64b0b25366f320034db",
+    "url": "/YH-Clothing/static/media/YousfitnessLogo.a5a86f17.png"
+  },
+  {
+    "revision": "4cf00bda865a0838149ed7fcc67c235a",
+    "url": "/YH-Clothing/static/media/if.4cf00bda.png"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/YH-Clothing/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "2c0663c1bd15ae55381e03234c909605",
+    "url": "/YH-Clothing/static/media/team1.2c0663c1.jpg"
+  },
+  {
+    "revision": "1e6799328bd18112242ccb5e05c77b27",
+    "url": "/YH-Clothing/static/media/team2.1e679932.jpg"
+  },
+  {
+    "revision": "c50c77465ea4c221cd3164fc11533c7a",
+    "url": "/YH-Clothing/static/media/team3.c50c7746.jpg"
+  },
+  {
+    "revision": "8b134bd92740a17d8ba8baaad87f067e",
+    "url": "/YH-Clothing/static/media/wave_1.8b134bd9.gif"
+  },
+  {
+    "revision": "bf19e90f6be153a541aea7b0d79216a9",
+    "url": "/YH-Clothing/static/media/wave_2.bf19e90f.png"
+  },
+  {
+    "revision": "26648e95ce1bd90d198e4b46d091f595",
+    "url": "/YH-Clothing/static/media/wave_3.26648e95.png"
+  }
+]);
